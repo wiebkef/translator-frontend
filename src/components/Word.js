25556df2da09a2556c6c4cfc510d20f50823c082
Wiebke <wiebke.feldt@gmail.com>
2023-02-13 13:45:57 +0100

    set up containers

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
 
 function Word() {
-  return <div>Word</div>;
+  return (
+    <Container className="text-start mt-4">
+      <h2>Schule</h2>
+    </Container>
+  );
 }
 
 export default Word;
--- src/components/WordsList.js
+++ src/components/WordsList.js
@@ -1,18 +1,24 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
 
 function WordsList(props) {
   const { words, resetWords } = props;
 
   return words.length > 0 ? (
-    <>
-      <ul>
+    <Container className="text-start mt-4">
+      <ul className="list-group">
         {words.map((word, i) => (
-          <li className={word.english ? "bold read" : "bold unread"} key={i}>
+          <li
+            className={`${
+              word.english ? "bold read" : "bold unread"
+            } list-group-item`}
+            key={i}
+          >
             {word.german}
           </li>
         ))}
       </ul>
-    </>
+    </Container>
   ) : (
     <h2>Noo Words</h2>
   );
