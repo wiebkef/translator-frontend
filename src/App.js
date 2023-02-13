656921004d13ca0e742883aa42fbca0031f0157d
Wiebke <wiebke.feldt@gmail.com>
2023-02-13 11:23:43 +0100

    set dummie content

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import "./App.css";
 import Searchbar from "./components/Searchbar";
 import WordsList from "./components/WordsList";
 import { Routes, Route, NavLink, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [words, setWords] = useState([
+    { german: "Schule", english: "school" },
+    { german: "Stuhl", english: "chair" },
+    { german: "Tisch", english: "table" },
+  ]);
+
   return (
     <div className="App">
       <Searchbar />
